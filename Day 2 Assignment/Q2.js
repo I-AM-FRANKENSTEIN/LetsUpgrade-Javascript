9967acae418420bb9b23108c841fe8aaa8a03331
I-AM-FRANKENSTEIN <46317426+I-AM-FRANKENSTEIN@users.noreply.github.com>
2021-01-26 19:43:13 +0530

    Add files via upload

---
+++ Day 2 Assignment/Q2.js
@@ -0,0 +1,26 @@
+console.log("\nQuestion 2:-")
+var marks=prompt("Enter your marks out of 100");
+if(marks>90){
+    console.log("\nAA")
+}
+else if(marks>80 && marks<=90){
+    console.log("\nAB")
+}
+else if(marks>70 && marks<=80){
+    console.log("\nBB")
+}
+else if(marks>60 && marks<=70){
+    console.log("\nBC")
+}
+else if(marks>50 && marks<=60){
+    console.log("\nCC")
+}
+else if(marks>40 && marks<=50){
+    console.log("\nCD")
+}
+else if(marks>30 && marks<=40){
+    console.log("\nDD")
+}
+else{
+    console.log("\nFF")
+}
